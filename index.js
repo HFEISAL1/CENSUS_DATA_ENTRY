88c9330746b854c64e4955db779bbd770bba3f06
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-06-25 02:59:44 -0700

    somechange have made

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
-async function login (email, password) {
-    const response = await zlFetch.post(loginEndpoint, {
-      auth: {
-        username: email,
-        password: password
-      },
-      body: { /*...*/ }
-    })
-  }
-  
- 
-  async function isLoggedIn () {
-    const token = store.get('token')
-    if (!token) return false
-  }
-  async function isLoggedIn () {
-    // ...
-    // Checks validity of token
-    const response = await zlFetch.post(loginEndpoint, {
-      auth: token,
-      body: { course: 'learn-javascript' }
-    })
-  }
-  async function isLoggedIn () {
-    // ...
-    // Saves token into localStorage again
-    const { token } = response.body
-    localStorage.setItem('token', token)
-  
-    return true
-  }
-  function isLoggedIn () {
-    // ...
-    const { token, user } = response.body
-    localStorage.setItem('user', user)
-  }
-  function logout () {
-    localStorage.removeItem('token')
-    localStorage.removeItem('user')
-  } 
-  function getinfo(){
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('close-btn-btn');
 
-    fetch(BASE_URL)
-    .then(resp => resp.json())
-    .then(data => {console.log(data);
+// event listeners
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
 
-        })
+
+// get meal list that matches with the ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
 }
-function submit(){
-    console.log("hello")
+
+
+// get recipe of the meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+// create a modal
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
