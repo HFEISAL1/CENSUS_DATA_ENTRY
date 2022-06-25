2c0ece3b50845cb0a60f944d9b6b8adb7f810140
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-06-25 12:29:21 -0700

    creating my function getmeallist

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('close-btn-btn');
-*/
+
 // event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
-
+*/
 
 // get meal list that matches with the ingredients
 function getMealList(){
